07acde5e525a92bfc32908046ad5d2977542e8bd
kothiga <kothiga2112@gmail.com>
2021-12-22 17:11:12 -0700

    Init of project files.

---
+++ src/iosTower/src/main.cpp
@@ -0,0 +1,20 @@
+/* ================================================================================
+ * Copyright: (C) 2022, SIRRL Social and Intelligent Robotics Research Laboratory, 
+ *     University of Waterloo, All rights reserved.
+ * 
+ * Authors: 
+ *     Austin Kothig <austin.kothig@uwaterloo.ca>
+ * 
+ * CopyPolicy: Released under the terms of the BSD 3-Clause License. 
+ *     See the accompanying LICENSE file for details.
+ * ================================================================================
+ */
+
+#include <iostream>
+
+int main (int argc, char **argv) {
+
+    std::cout << "Hello world" << std::endl;
+
+    return 0;
+}
